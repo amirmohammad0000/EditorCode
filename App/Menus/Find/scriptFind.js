5beea8930e614b7373d72d97dca8e66f3c66e56c
amirmohammad <koshkian1000@gmail.com>
2021-06-13 13:02:33 +0430

    Delete App directory

--- App/Menus/Find/scriptFind.js
+++
@@ -1,6 +0,0 @@
-const ElectronFind = require("electron");
-let inputFind = document.querySelector("#inputFind");
-
-inputFind.addEventListener("keyup", () => {
-    ElectronFind.ipcRenderer.send("Find", inputFind.value);
-});
